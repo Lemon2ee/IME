dc8e06f99628cb3128f4379c35fafb0a57243c9c
JerryGCDing <jerrydty@outlook.com>
2021-10-26 21:53:25 -0400

    Add implementations to model

---
+++ src/model/ImageModelImpl.java
@@ -0,0 +1,57 @@
+package model;
+
+import com.sun.source.tree.Tree;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImageModelImpl implements ImageModel {
+  private Map<String, Color[][]> operationQueue;
+
+  public ImageModelImpl() {
+    operationQueue = new HashMap<String, Color[][]>();
+  }
+
+  @Override
+  public void load(String name, Color[][] image) throws IllegalArgumentException {
+    this.operationQueue.put(name, image);
+  }
+
+  @Override
+  public void greyScale(String origin, String destination, GreyScaleValue op)
+          throws IllegalArgumentException {
+    Color[][] src = getSourceImage(origin);
+    ...
+  }
+
+  @Override
+  public void changeBrightness(String origin, String destination, int value)
+          throws IllegalArgumentException {
+    Color[][] src = getSourceImage(origin);
+    ...
+  }
+
+  @Override
+  public void flip(String origin, String destination, FlipDirection fd)
+          throws IllegalArgumentException {
+    Color[][] src = getSourceImage(origin);
+    ...
+  }
+
+  /**
+   * Get the source image from the queue with given name.
+   *
+   * @param origin the name of the source image as a String
+   * @return the 2d Color array of the source image
+   * @throws IllegalArgumentException if the provided name cannot be found
+   */
+  private Color[][] getSourceImage(String origin) throws IllegalArgumentException {
+    Color[][] result = this.operationQueue.get(origin);
+    if (result == null) {
+      throw new IllegalArgumentException("The provided source image is invalid.");
+    }
+
+    return result;
+  }
+}
