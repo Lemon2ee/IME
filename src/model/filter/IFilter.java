e3d6ef7a556022f16421ecc1046df23caf7eff28
JerryGCDing <jerrydty@outlook.com>
2021-11-03 23:40:28 -0400

    Filter function initial commit

---
+++ src/model/filter/IFilter.java
@@ -0,0 +1,19 @@
+package model.filter;
+
+import java.awt.Color;
+
+import model.image.ImageModel;
+
+/**
+ * The interface represents a filer.The filter has only one method to execute on the given
+ * 2d array of Color.
+ */
+public interface IFilter {
+  /**
+   * Execute the filter kernel operation on the given image data.
+   *
+   * @param origin the original image to filter on as ImageModel
+   * @return new image after filtering as ImageModel
+   */
+  ImageModel execute(ImageModel origin);
+}
